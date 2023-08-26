af6e76638bc2af4fc32c484e583002e17ad69b53
Cristiano <cris.moraes.ds@gmail.com>
2023-08-26 18:31:32 -0300

    feat: router layer

--- src/index.php
+++ src/index.php
@@ -14,16 +14,49 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 /**
- * Configure routes.
- *
- * By configuring the routes we are telling which pages a PHP code
- * will be fired.
+ * Configure the HTTP routes of our application using a simple system.
  * 
- * We will configure, for example, the pages:
+ * For example, we can say that if the user enters the route (URI)
+ * `/donation` with the `POST` method, it should trigger the file
+ * at `./donation/create.php`, which will create a new donation.
  * 
- * - `/donations` to trigger a file in `./donations` directory.
- * - `/webhooks` to trigger a file in `./webhooks` directory.
+ * On a routing system, you will configure user pages.
  * 
- * Go to this `router.php` file to see more.
+ * Example routing systems:
+ *   - Laravel: https://laravel.com/docs/10.x/routing
  */
-require_once __DIR__ . "/router.php";
+
+// Normalize the Request URI.
+// Removes leading and trailing whitespace and slashes from the URI.
+$requestUri = trim($_SERVER["REQUEST_URI"], "/\n\r\t\v\x00");
+
+// The HTTP request method like `GET` and `POST`.
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+// The route arguments, like some ID.
+$routeArgs = explode("/", $requestUri);
+
+// Endpoint: POST `/donation`
+$hasRouteMatch = $requestUri === "donation"
+    && $requestMethod === "POST";
+
+if ($hasRouteMatch) {
+    require_once __DIR__ . "/route/donation/create.php";
+    exit;
+}
+
+// Endpoint: GET `/donation/{donationId}`
+$hasRouteMatch = ! empty($routeArgs[0])
+    && $routeArgs[0] === "donation"
+    && ! empty($routeArgs[1])
+    && $requestMethod === "GET";
+
+if ($hasRouteMatch) {
+    require_once __DIR__ . "/route/donation/getOne.php";
+    exit;
+}
+
+/**
+ * Indicate a 404 error to PHP built-in server.
+ */
+return false;
