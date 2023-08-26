c8ebb04412565179cdf91a71cb02e96a9c6725d9
criskell <96352451+criskell@users.noreply.github.com>
2023-08-26 19:44:53 -0300

    feat(donation): add GET endpoint (#8)

--- src/pages/donation/GET.php
+++ src/pages/donation/GET.php
@@ -6,7 +6,28 @@ return function (int $donationId): void
     // Load the database.
     $db = require_once __DIR__ . "/../../db.php";
 
-    $donations = $db->query("SELECT * FROM Donation");
+    $donation = findDonationById($db, $donationId);
 
-    var_dump($donations);
+    if (empty($donation)) {
+        http_response_code(404);
+        exit;
+    }
+
+    http_response_code(200);
+    echo json_encode($donation);
 };
+
+/**
+ * Return donation by ID or a empty array if not found.
+ */
+function findDonationById(PDO $db, int $donationId): array
+{
+    $query = $db->prepare("SELECT * FROM Donation WHERE id = ?");
+    $query->execute([$donationId]);
+
+    $result = $query->fetch();
+
+    if ($result === false) return [];
+
+    return $result;
+}
