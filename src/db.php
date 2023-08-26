793ba8beebb2e5f249b63b5acb5b1b75d358a53d
Cristiano <cris.moraes.ds@gmail.com>
2023-08-26 19:28:49 -0300

    fix: database connection and schema

--- src/db.php
+++ src/db.php
@@ -7,17 +7,16 @@
 static $db;
 
 if (is_null($db)) {
+    $path = __DIR__ . "/../db.sqlite";
+
     // @see https://www.php.net/manual/en/ref.pdo-sqlite.connection.php#105350
     $db = new PDO(
-        "sqlite::memory:",
-        null,
-        null,
-        [PDO::ATTR_PERSISTENT => true]
+        "sqlite:" . $path,
     );
 
     // Add necessary changes to database.
     $db->query("
-        CREATE TABLE IF NOT EXISTS (
+        CREATE TABLE IF NOT EXISTS Donation (
             id INTEGER PRIMARY KEY,
             value INTEGER,
             comment TEXT,
